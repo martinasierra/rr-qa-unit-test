4a9afc0a7519624910f8109a795e41bf79281115
Martina Sierra <martinasierra94@gmail.com>
2021-04-25 19:33:24 -0300

    Add division and tablaMultiplicar tests

--- functions.js
+++ functions.js
@@ -35,9 +35,9 @@ const division = (dividendo, divisor) => {
 //6
 const tablaMultiplicar = (multiplicando, multiplicador) => {
   if (multiplicador === undefined) multiplicador = 10;
-  let table = []
+  let table = [];
   for (let i = 0; i <= multiplicador; i++){
-    table.push(multiplicando * i)
+    table.push(multiplicando * i);
   }
   return table;
 }  
--- functions.test.js
+++ functions.test.js
@@ -41,7 +41,7 @@ describe ("suma should take two numbers as arguments and return the sum of them"
 
 describe ("resta should take two numbers as arguments and return the subtraction of them", () => {
 
-    test('given 6 and 1 as argumentes, returns 5', () => {
+    test('given 6 and 1 as arguments, returns 5', () => {
         expect (functions.resta(6,1)).toBe(5);
     });
 
@@ -53,11 +53,11 @@ describe ("resta should take two numbers as arguments and return the subtraction
 
 describe ("multiplicacion should take two numbers as arguments and return the product of them", () => {
 
-    test('given 6 and 3 as argumentes, returns 18', () => {
+    test('given 6 and 3 as arguments, returns 18', () => {
         expect (functions.multiplicacion(6,3)).toBe(18);
     });
 
-    test('given 6 and 0 as argumentes, returns 0', () => {
+    test('given 6 and 0 as arguments, returns 0', () => {
         expect (functions.multiplicacion(6,0)).toBe(0);
     });
 
@@ -66,4 +66,44 @@ describe ("multiplicacion should take two numbers as arguments and return the pr
     }); */
 });
 
+describe ("division should take two numbers as arguments and return the product of them", () => {
 
+    test('given 6 and 3 as arguments, returns 2', () => {
+        expect (functions.division(6,3)).toBe(2);
+    });
+
+    test('given 6 and 0 as arguments, returns No se puede dividir por 0', () => {
+        expect (functions.division(6,0)).toMatch('No se puede dividir por 0');
+    });
+
+    test('given 7 and 2 as arguments, returns 3.5', () => {
+        expect (functions.division(7,2)).toBe(3.5);
+    });
+
+});
+
+describe ("tablaMultiplicacion should take two numbers and return an array of numbers with multiples of the first argument up to the second argument", () => {
+
+    test('given 3 and 8 as arguments, returns 0,3,6,9,12,15,18,21,24', () => {
+        expect (functions.tablaMultiplicar(3,8)).toContain(0,3,6,9,12,15,18,21,24);
+    });    
+
+    test('given 3 and 9 as arguments, returns 0,3,6,9,12,15,18,21,24,27,30', () => {
+        expect (functions.tablaMultiplicar(3,)).toContain(0,3,6,9,12,15,18,21,24,27,30);
+    });
+});
+
+describe ("", () => {
+
+    
+    
+});
+
+describe ("", () => {
+    
+});
+
+describe ("", () => {
+    
+});
+// npm run test-coverage
