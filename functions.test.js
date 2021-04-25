1638501d80cc1577ba49e1c177d720d6c665f8b5
Martina Sierra <martinasierra94@gmail.com>
2021-04-25 15:15:50 -0300

    Add suma tests and change descriptions

--- functions.test.js
+++ functions.test.js
@@ -1,17 +1,41 @@
 import * as functions from "./functions";
 
-describe ("texto test", () => {
+describe ("texto should take any argument and returns it", () => {
 
-    test('enter a text and return it/enter getting started returns getting started', () => {
+    test('given getting started as argument, returns getting started', () => {
         expect (functions.texto('Getting started')).toBe('Getting started');
     });
 
-    test('enter a number a returns it', () => {
-        expect (functions.texto(123456)).toBe(123456);
+    test('given 1234 as argument, returns 1234', () => {
+        expect (functions.texto(1234)).toBe(1234);
     });
 
-    test('enter undefined a returns it', () => {
-        expect (functions.texto(undefined)).toBe(undefined);
+    test('given undefined as argument, returns undefined', () => {
+        expect (functions.texto(undefined)).toBeUndefined(undefined);
+    });
+
+    test('given an empty string as argument, returns it', () => {
+        expect (functions.texto('')).toBe('');
+    });
+
+});
+
+describe ("suma should take two numbers as arguments and return the sum of them", () => {
+    
+    test('given 4 and 4 as arguments, returns 8', () => {
+        expect (functions.suma(4,4)).toBe(8);
+    });
+
+    test('given 1.5 and 0.5 as arguments, returns 2', () => {
+        expect (functions.suma(1.5,0.5)).toBe(2);
+    });
+
+    test('given bici and senda as arguments, returns bicisenda', () => {
+        expect (functions.suma('hola','chau')).toBe('bicisenda');
+    });
+
+    test('add null + 2 equals 2', () => {
+        expect (functions.suma(null,2)).toBe(2);
     });
 });
 
